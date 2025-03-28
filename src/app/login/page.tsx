e7535bf9244d8f140709783f1c37c3d1d7180e02
kadelcode <kadelcode@gmail.com>
2025-03-28 17:19:05 +0100

    Update page structure

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,10 +5,12 @@ import { z } from "zod"; // Library for schema definition and validation
 import { zodResolver } from "@hookform/resolvers/zod"; // Integration between react-hook-form and Zod
 import { useAuthStore } from "@/store/authStore"; // Custom hook for authentication state management
 import { useRouter } from "next/navigation"; // Hook for navigation in Next.js
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Link from "next/link";
 import { Loader2, Eye, EyeOff } from "lucide-react";
+
+import LoginSkeleton from "@/components/skeletons/LoginSkeleton";
 
 // Define Zod schema for login form validation
 const loginSchema = z.object({
@@ -21,6 +23,7 @@ export default function LoginPage() {
   const { login } = useAuthStore(); // Get the login function from the auth store
   const router = useRouter(); // Initialize the router for navigation
   const [showPassword, setShowPassword] = useState(false);
+  const [loading, isLoading] = useState(true);
 
   // Use react-hook-form with Zod validation
   const {
@@ -38,74 +41,83 @@ export default function LoginPage() {
     router.push("/dashboard"); // Navigate to the dashboard page after successful login
   };
 
+  // Simulate loading time
+  useEffect(() => {
+    setTimeout(() => isLoading(false), 1500)
+  }, []);
+
   // Render the login form
   return (
     <section className="flex items-center h-screen">
-    <div className="w-96 mx-auto p-5 border rounded">
-      <h2 className="text-xl font-bold">Login To Your Account</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-            <input
-            type="email"
-            placeholder="Email"
-            {...register("email")} // Register the email input field
-            className={`bg-white focus:outline-0 focus:border-green-400 border rounded-md p-2 w-full mt-5 transition-colors 0.3 ${errors.email ? " border-red-500 focus:outline-0 focus:border-red-500" : ""}`}
-            />
-            {/* Display email error message */}
-            {errors.email && <small className="text-red-500">{errors.email.message}</small>}
+        { loading ? (
+            <LoginSkeleton />
+        ) : (
+            <div className="w-96 mx-auto p-5 border rounded">
+            <h2 className="text-xl font-bold">Login To Your Account</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <input
+                    type="email"
+                    placeholder="Email"
+                    {...register("email")} // Register the email input field
+                    className={`bg-white focus:outline-0 focus:border-green-400 border rounded-md p-2 w-full mt-5 transition-colors 0.3 ${errors.email ? " border-red-500 focus:outline-0 focus:border-red-500" : ""}`}
+                    />
+                    {/* Display email error message */}
+                    {errors.email && <small className="text-red-500">{errors.email.message}</small>}
+                </div>
+
+                <div className="relative">
+                    <input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                    {...register("password")} // Register the password input field
+                    className={`bg-white focus:outline-0 focus:border-green-400 border rounded-md p-2 w-full mt-5 transition-colors 0.3 ${errors.password ? "focus:outline-0 border-red-500 focus:border-red-500": ""}`}
+                    />
+                    <button
+                    type="button" // Prevents triggering validation/submission
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-3 top-8 text-gray-500"
+                    >
+                        {showPassword ? <EyeOff width={20} height={20} /> : <Eye width={20} height={20} />}
+                    </button>
+                    {/* Display password error message */}
+                    {errors.password && (
+                    <small className="text-red-500">{errors.password.message}</small>
+                    )}
+                </div>
+                
+
+                <button
+                type="submit"
+                className={`bg-green-400 hover:bg-green-500 text-white px-4 py-2 mt-5 w-full rounded-md 
+                    ${isSubmitting ? "opacity-50 cursor-not-allowed" : "cursor-pointer"}
+                `}
+                disabled={isSubmitting} // Disable button during submission
+                >
+                {/* Display loading text during submission */}
+                {isSubmitting ? 
+                    <div className="flex items-center justify-center">
+                        <Loader2 className="animate-spin mr-2" />
+                        Logging in...
+                    </div> : 
+                    "Login"
+                }
+                </button>
+            </form>
+
+            {/* Forgot Password Link */}
+            <div className="mt-3 text-center">
+                <a href="/forgot-password" className="text-blue-500 hover:underline">
+                Forgot Password?
+                </a>
+            </div>
+
+            {/* Link to Register Div */}
+            <div className="text-center mt-5">
+                    <span>Don't have an account? <Link className="text-blue-500 hover:underline" href="/register">Register</Link></span>
+            </div>
         </div>
-
-        <div className="relative">
-            <input
-            type={showPassword ? "text" : "password"}
-            placeholder="Password"
-            {...register("password")} // Register the password input field
-            className={`bg-white focus:outline-0 focus:border-green-400 border rounded-md p-2 w-full mt-5 transition-colors 0.3 ${errors.password ? "focus:outline-0 border-red-500 focus:border-red-500": ""}`}
-            />
-            <button
-              type="button" // Prevents triggering validation/submission
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute right-3 top-8 text-gray-500"
-            >
-                {showPassword ? <EyeOff width={20} height={20} /> : <Eye width={20} height={20} />}
-            </button>
-            {/* Display password error message */}
-            {errors.password && (
-            <small className="text-red-500">{errors.password.message}</small>
-            )}
-        </div>
-        
-
-        <button
-          type="submit"
-          className={`bg-green-400 hover:bg-green-500 text-white px-4 py-2 mt-5 w-full rounded-md 
-            ${isSubmitting ? "opacity-50 cursor-not-allowed" : "cursor-pointer"}
-          `}
-          disabled={isSubmitting} // Disable button during submission
-        >
-          {/* Display loading text during submission */}
-          {isSubmitting ? 
-            <div className="flex items-center justify-center">
-                <Loader2 className="animate-spin mr-2" />
-                Logging in...
-            </div> : 
-            "Login"
-          }
-        </button>
-      </form>
-
-      {/* Forgot Password Link */}
-      <div className="mt-3 text-center">
-        <a href="/forgot-password" className="text-blue-500 hover:underline">
-          Forgot Password?
-        </a>
-      </div>
-
-      {/* Link to Register Div */}
-      <div className="text-center mt-5">
-            <span>Don't have an account? <Link className="text-blue-500 hover:underline" href="/register">Register</Link></span>
-      </div>
-    </div>
+        )}
     </section>
   );
 }
