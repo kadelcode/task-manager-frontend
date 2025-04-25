5c6b23f96e98769b145bc0ba83f4034363f19a4b
kadelcode <kadelcode@gmail.com>
2025-04-25 05:18:34 +0100

    Update styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -113,28 +113,33 @@ export default function Navbar() {
                 <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
                     <input type="text" placeholder="Task Title"
                       name="title"
+                      style={{ border: '1px solid black' }}
                       className="p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" required 
                     />
                     <textarea placeholder="Task Description"
                       name="description"
+                      style={{ border: '1px solid black' }}
                       className="p-2 border rounded-md h-24 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     ></textarea>
 
                     {/* Priority & Due Date */}
                     <div className="flex gap-4">
-                        <select name="priority" className="flex-1 p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+                        <select name="priority" 
+                          style={{ border: '1px solid black', color: 'black' }}
+                          className="flex-1 p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
                             <option value="low">Low Priority</option>
                             <option value="medium">Medium Priority</option>
                             <option value="high">High Priority</option>
                         </select>
                         <input type="date" name="dueDate"
+                          style={{ border: '1px solid black', color: 'black' }}
                           className="flex-1 p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                           required
                         />
                     </div>
 
                     {/* Submit Button */}
-                    <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600">
+                    <button type="submit" className="w-full bg-[#2b7fff] text-white py-2 rounded-md hover:bg-[#155dfc]">
                         { isLoading ?
                         <div className="flex justify-center items-center gap-2">
                             <Loader2 className="animate-spin" />
