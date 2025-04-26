0b6a4d1d08f1a8454f250dc4b4f727d746f2a232
kadelcode <kadelcode@gmail.com>
2025-04-26 14:03:42 +0100

    Update code to use TaskFromAPI interface

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Task } from "@/types/taskTypes";
+import { Task, TaskFromAPI } from "@/types/taskTypes";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/tasks";
 
@@ -14,30 +14,30 @@ const getAuthHeaders = () => {
 }
 
 const taskService = {
-    getTasks: async (): Promise<Task[]> => {
+    getTasks: async (): Promise<TaskFromAPI[]> => {
         const response = await axios.get(`${API_URL}/tasks/`, getAuthHeaders());
         return response.data;
     },
 
-    getTaskById: async (taskId: string): Promise<Task> => {
+    getTaskById: async (taskId: string): Promise<TaskFromAPI> => {
         const response = await axios.get(`${API_URL}/tasks/${taskId}`)
         return response.data;
     },
 
-    createTask: async (taskData: Omit<Task, "id">): Promise<Task> => {
+    createTask: async (taskData: Omit<Task, "id">): Promise<TaskFromAPI> => {
         const response = await axios.post(`${API_URL}/tasks`, taskData, getAuthHeaders());
         return response.data;
     },
 
     updateTask: async (
         taskId: string, 
-        taskData: Partial<Task>): Promise<Task> => {
-            const response = await axios.put(`${API_URL}/tasks/${taskId}`, taskData);
-            return response.data;
+        taskData: Partial<Task>): Promise<TaskFromAPI> => {
+            const response = await axios.put(`${API_URL}/tasks/${taskId}`, taskData, getAuthHeaders());
+            return response.data.task;
     },
 
     deleteTask: async (taskId: string): Promise<{message: string}> => {
-        const response = await axios.delete(`${API_URL}/tasks/${taskId}`);
+        const response = await axios.delete(`${API_URL}/tasks/${taskId}`, getAuthHeaders());
         return response.data;
     }
 };
