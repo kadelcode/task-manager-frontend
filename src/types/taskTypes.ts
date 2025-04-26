65148383e95601b8649f3b7e2ec400eb018a9ac4
kadelcode <kadelcode@gmail.com>
2025-04-26 13:53:59 +0100

    Add TaskFromAPI interface

--- src/types/taskTypes.ts
+++ src/types/taskTypes.ts
@@ -1,6 +1,7 @@
-// Define the Task interface to specify the structure of a task object
-export interface Task {
-    id: string; // Unique identifier for the task
+// This interface defines the structure of a task object as it is expected
+// to be received from an API
+export interface TaskFromAPI {
+    _id: string; // Unique identifier for the task
     title: string; // Title of the task
     description: string; // Description of the task
     status: "todo" | "in-progress" | "done" | "overdue"; // Status of the task
@@ -8,3 +9,8 @@ export interface Task {
     dueDate: string; // Due date of the task
     assignedTo: string; // Person assigned to the task
 };
+
+// This interface extends from TaskFromAPI interface but omits the _id property
+export interface Task extends Omit<TaskFromAPI, "_id"> {
+    id: string;
+}
